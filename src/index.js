6085d6a9125f7eeb6bda7287a55b58c9e2b2124c
jkurguzova <jkurguzova@inbox.ru>
2020-10-16 16:52:41 +0300

    feat: implement brackets validity

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+if (str.length <= 1)
+return false;
+let matchingOpeningBracket;
+let ch;
+let stack = [];
+bracketsConfig = bracketsConfig.reduce((acc, val) => acc.concat(val), []);
+let openingBrackets = bracketsConfig.filter((e, i) => !(i % 2));
+let closingBrackets = bracketsConfig.filter((e, i) => (i % 2));
+for (let i = 0; i < str.length; i++) {
+    ch = str[i];
+    if (closingBrackets.indexOf(ch) > -1 && closingBrackets.indexOf(ch) != openingBrackets.indexOf(ch)) {
+        matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(ch)]
+        if (stack.length == 0 || (stack.pop() != matchingOpeningBracket)) {
+            return false;}
+    } else if (closingBrackets.indexOf(ch) > -1 && closingBrackets.indexOf(ch) == openingBrackets.indexOf(ch)) {
+        if (stack.indexOf(ch) > -1) {
+            matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(ch)]
+            if (stack.length == 0 || (stack.pop() != matchingOpeningBracket)) {
+                return false;}
+        } else { stack.push(ch); }
+    } else {
+        stack.push(ch);
+    }
+};
+
+return (stack.length == 0);
 }
+
